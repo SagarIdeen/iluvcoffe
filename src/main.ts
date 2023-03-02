2902eddccd42a5543d11a2e35015846ef5446ced
Sagarideen <isagarideen@gmail.com>
2023-03-02 10:26:18 +0530

    Initial

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  //swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Swagger example')
+    .setDescription('Sample API description')
+    .setVersion('1.0')
+    .addTag('Test')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  await app.listen(3000);
+}
+bootstrap();
